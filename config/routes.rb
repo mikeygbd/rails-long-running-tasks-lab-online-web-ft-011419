321729b7b71d6c98734f66613807aa6d5ff68f2f
mikeygbd <mikey.soares@gmail.com>
2019-05-06 11:52:44 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+resources :artists, :songs
+post 'songs/upload', to: 'songs#upload'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
